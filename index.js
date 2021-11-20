80aa484a3a9edfc295818754da930c0d006172c9
minhvuong123 <minhvuong12222@gmail.com>
2021-11-20 12:14:33 +0700

    init server

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '100mb'}));
+app.use('/static', express.static('public'));
+
+// routes
+const users = require('./routes/user/user.route');
+
+app.use('/users', users);
+
+mongoose.connect('mongodb://localhost/socialApp', { useNewUrlParser: true, useUnifiedTopology: true });
+const connect = mongoose.connection;
+connect.on('error', function(){
+  console.log('Mongodb connect to fail !');
+});
+connect.on('open', function(){
+  console.log('Mongodb connected...');
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on ' + PORT);
+})
---
+++ models/user/user.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: true
+    },
+    last_name: {
+      type: String,
+      required: true
+    },
+    avatar: {
+      type: String,
+      default: ''
+    },
+    background_image: {
+      type: String,
+      default: ''
+    },
+    email_phone: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    birthday: {
+      type: String,
+      required: true
+    },
+    background_color: {
+      type: String,
+      default: ''
+    }
+  },
+  { 
+    timestamps: true 
+  }
+);
+
+module.exports = mongoose.model('User', userSchema);
