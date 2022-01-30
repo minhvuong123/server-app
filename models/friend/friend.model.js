a30891b74a4303f3eb1684fc40a34c2fade24af7
minhvuong123 <minhvuong12222@gmail.com>
2022-01-31 00:22:56 +0700

    update friend and validation model

--- models/friend/friend.model.js
+++ models/friend/friend.model.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose');
 
 const friendSchema = new mongoose.Schema(
   {
-    friendId: {
-      type: String,
-      required: true
-    },
     first_name: {
       type: String,
       required: true
